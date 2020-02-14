becd8db7735c35eb10f7ab69e2359ef4ca5ff0ca
Quynh <quynh@Quynhs-MacBook-Pro.local>
2020-02-14 11:47:30 +0700

    ball

--- Ball/Core/AppDelegate.swift
+++ Ball/Core/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let mainVC = DrawOViewController()
+        let mainVC = DropBallViewController()
         let navigation = UINavigationController(rootViewController: mainVC)
         
         window?.rootViewController = mainVC
